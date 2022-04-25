e8a02e1d5772a6b4f9a6606e59d4760a2de1b849
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-25 17:12:42 -0300

    simuladorRendimento

--- src/main/java/com/trybe/acc/java/SimuladorRendimento.java
+++ src/main/java/com/trybe/acc/java/SimuladorRendimento.java
@@ -2,5 +2,11 @@ package com.trybe.acc.java;
 
 
 public class SimuladorRendimento {
-
+  public static void main(String[] args) {
+    SimuladorRendimento obj = new SimuladorRendimento();
+    double inicial = 500;
+    double objetivo = 5000;
+    System.out.println("Com " + inicial + " seu rendimento será " + objetivo + " no ano "
+        + obj.simularRendimentoAnos(inicial, objetivo));
+  }
 }
